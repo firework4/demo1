a234eaf5f18f9db70f108443c53d04d471e76574
firework4 <276384020@qq.com>
2023-03-21 11:13:14 +0800

    f

--- src/main/java/com/example/mytes/mapper/Studentmapper.java
+++ src/main/java/com/example/mytes/mapper/Studentmapper.java
@@ -1,10 +1,13 @@
 package com.example.mytes.mapper;
 
 import com.example.mytes.entity.Student;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
+@Mapper
 public interface Studentmapper {
     List<Student> getOne(Integer id);
 
